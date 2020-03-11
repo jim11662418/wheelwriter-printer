ef0cbdc91bf6742927bedfcca338bbcbc928ffc3
Jim <51113579+jim11662418@users.noreply.github.com>
2020-03-11 08:45:57 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+//---------------------------------------------------------------------------------------------------------------------------------
+// Copyright Â© 2020 Jim Loos
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------------------------------------------------------
+
 //------------------------------------------------------------------------------------------
 // Print characters received from the parallel port, serial port or PS/2 keyboard on the Wheelwriter.
 //
@@ -471,7 +487,7 @@ void print_character(unsigned char charToPrint) {
 // the Delete key acts as the Correction key (erases the last character)
 //
 // special characters on the Wheelwriter keyboard but not on the ps/2 keyboard:
-//  ¢, §, ², ³, ¶, º, ¼, ½, ±
+//  Â¢, Â§, Â², Â³, Â¶, Âº, Â¼, Â½, Â±
 //
 // characters on the ps/2 keyboard but not on the printwheel:
 //  { (left curly bracket), } (right curly bracket), ` (back tick), ~ (tilde)
